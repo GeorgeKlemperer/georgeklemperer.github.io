e52ad3d8b31a38097bf7c4601bd17dae0773658e
GeorgeKlemperer <124908931+GeorgeKlemperer@users.noreply.github.com>
2023-03-13 22:58:56 +0000

    Delete my-script.js

--- my-script.js
+++
@@ -1,5 +0,0 @@
-console.log("JavaScript is working!")
-
-const date = new Date();
-const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-document.getElementById("date").innerHTML = days[date.getDay()]+" "+date.getDate()+"/"+(date.getMonth()+1)+"/"+date.getFullYear();
