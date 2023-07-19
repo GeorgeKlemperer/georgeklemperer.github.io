350959eae70ebde19652ff002825adb8e0b91944
GeorgeKlemperer <george@klemperer.co.uk>
2023-07-19 18:53:52 +0100

    deleted redundant code

--- my-scripts.js
+++ my-scripts.js
@@ -47,34 +47,3 @@ const titleToType = "R. George Klemperer";
 
 // Typing effect for the main title
 type();
-
-// When the user clicks on info button, open the popup
-function popupFunction1() {
-  var popup = document.getElementById("myPopup1");
-  popup.classList.toggle("reveal");
-}
-
-function popupFunction2() {
-  var popup = document.getElementById("myPopup2");
-  popup.classList.toggle("reveal");
-}
-
-function popupFunction3() {
-  var popup = document.getElementById("myPopup3");
-  popup.classList.toggle("reveal");
-}
-
-function popupFunction4() {
-  var popup = document.getElementById("myPopup4");
-  popup.classList.toggle("reveal");
-}
-
-function popupFunction5() {
-  var popup = document.getElementById("myPopup5");
-  popup.classList.toggle("reveal");
-}
-
-function popupFunction6() {
-  var popup = document.getElementById("myPopup6");
-  popup.classList.toggle("reveal");
-}
